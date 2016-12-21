97157700003bc5939f25894678f0ee77f0f23014
Hongzhi Li <hongzhi.li@microsoft.com>
2016-12-20 16:39:39 -0800

    fix an error in pxe server

--- src/ClusterBootstrap/pxe-kubelet/www/cloud-config/install_os_kubelet.sh
+++ src/ClusterBootstrap/pxe-kubelet/www/cloud-config/install_os_kubelet.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 wget http://192.168.1.20/pxe-coreos-kube.yml
-sudo coreos-cloudinit --from-file=pxe-coreos-kube.yml
+sudo coreos-install -d /dev/sda -c pxe-coreos-kube.yml -b http://192.168.1.20/coreos
 
 sudo shutdown -h now
 
--- src/ClusterBootstrap/pxe-kubelet/www/cloud-config/install_os_master.sh
+++ src/ClusterBootstrap/pxe-kubelet/www/cloud-config/install_os_master.sh
@@ -1,37 +1,5 @@
 #!/bin/sh
-
-#wget http://192.168.1.20/pxe-cloud-config1.yml
 wget http://192.168.1.20/pxe-kubemaster.yml
-
-#sudo coreos-cloudinit --from-file=pxe-cloud-config1.yml
-
-
-#sudo coreos-install -d /dev/sda -c pxe-cloud-config1.yml -b http://192.168.1.20/coreos
 sudo coreos-install -d /dev/sda -c pxe-kubemaster.yml -b http://192.168.1.20/coreos
-
-
-#sudo mount /dev/sda9 /mnt
-
-
-#df -h
-
-#sudo mkdir -p /mnt/etc/kubernetes
-#sudo mkdir -p /mnt/srv/kubernetes
-
-#wget -q -O - http://192.168.1.20/kubeconfig | base64 -d > "kubeconfig.json"
-#sudo mv kubeconfig.json /mnt/srv/kubernetes/kubeconfig.json
-
-#wget http://192.168.1.20/hyperkube.tar.gz
-#sudo mkdir -p /mnt/opt/init
-#sudo mv hyperkube.tar.gz /mnt/opt/init
-
-
-#wget http://192.168.1.20/nvidia_driver.tar
-#sudo mv nvidia_driver.tar /mnt/opt/init
-
-#wget http://192.168.1.20/docker_kubelet.sh
-#sudo mv docker_kubelet.sh /mnt/opt/init
-
-#sudo reboot
 sudo shutdown -h now
 
