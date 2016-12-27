87ba2ae9233445c9915198bcba4e78542c750b7b
Hongzhi Li <hongzhi.li@microsoft.com>
2016-12-27 12:15:51 -0800

    update pxe server

--- src/ClusterBootstrap/pxe-kubelet/copy_html_data.sh
+++ src/ClusterBootstrap/pxe-kubelet/copy_html_data.sh
@@ -10,10 +10,10 @@ cp -R ~/DLWorkspace/src/ClusterBootstrap/pxe-kubelet/tftp/* /var/lib/tftpboot/
 cd /var/www/html
 mkdir coreos
 cd coreos
-mkdir 1185.3.0
-cd 1185.3.0
-wget -q http://ccsdatarepo.westus.cloudapp.azure.com/data/coreos/coreos/1185.3.0/coreos_production_image.bin.bz2
-wget -q http://ccsdatarepo.westus.cloudapp.azure.com/data/coreos/coreos/1185.3.0/coreos_production_image.bin.bz2.sig
+mkdir 1185.5.0
+cd 1185.5.0
+wget -q https://stable.release.core-os.net/amd64-usr/1185.5.0/coreos_production_image.bin.bz2
+wget -q https://stable.release.core-os.net/amd64-usr/1185.5.0/coreos_production_image.bin.bz2.sig
 
 cd /var/lib/tftpboot/
 wget -q https://stable.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz 
