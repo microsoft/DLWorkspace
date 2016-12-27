9880cd68e9908a53e45360e5fdffabdff514a13c
Hongzhi Li <hongzhi.li@microsoft.com>
2016-12-27 12:17:00 -0800

    fix bugs: generate Persistent Volume

--- src/storage/gen_pv_pvc.py
+++ src/storage/gen_pv_pvc.py
@@ -4,22 +4,20 @@ import os
 import sys
 
 
-def main(argv):
+def GenStorageClaims(Id,storagePath, outputPath):
     ENV = Environment(loader=FileSystemLoader(os.path.dirname(os.path.abspath(__file__))))
-    tag=argv[1]
-    Id=argv[2]
     pvtemplatePath="pv.yaml.template"
     pvctemplatePath="pvc.yaml.template"
 
-    outputPath=argv[3]
-    localPath = "/dlws-data/"+tag+"/"+Id
-    if not os.path.exists(localPath):
-        os.makedirs(localPath)
+    localPath = "/dlws-data/"+storagePath
 
-    serverPath = "/mnt/data/"+tag+"/"+Id
+    if not os.path.exists(localPath):
+        raise ValueError("path does not exist on storage server, cannot create Persistent Volume")
+
+    serverPath = "/mnt/data/"+storagePath
 
     pv={}
-    pv["name"] = "pv-"+tag+"-"+Id
+    pv["name"] = Id
     pv["capacity"] = "2Gi"
     pv["server"]="10.196.44.241"
     pv["path"]=serverPath
@@ -34,7 +32,14 @@ def main(argv):
     template = ENV.get_template(pvctemplatePath)
     with open(outputPath+"/pvc-"+Id+".yaml", 'w') as f:
         f.write(template.render(pv=pv))
+    return (outputPath+"/pv-"+Id+".yaml",outputPath+"/pvc-"+Id+".yaml")
 
+
+def main(argv):
+    tag=argv[1]
+    Id=argv[2]
+    outputPath=argv[3]
+    GenStorageClaims(tag,Id,outputPath)
 
 if __name__ == "__main__":
     main(sys.argv)
