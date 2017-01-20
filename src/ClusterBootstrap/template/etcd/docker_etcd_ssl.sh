4f67d7f704ab7800265a7733fd1d0f91a2838cac
Hongzhi Li <hongzhi.li@microsoft.com>
2017-01-20 10:48:31 -0800

    switch to kubernetes 1.5.2

--- src/ClusterBootstrap/template/etcd/docker_etcd_ssl.sh
+++ src/ClusterBootstrap/template/etcd/docker_etcd_ssl.sh
@@ -1,14 +1,14 @@
-export HostIP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}') && \
+#export HostIP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}') && \
 #curl -w "\n" 'https://discovery.etcd.io/new?size=3'
 
 docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -v /etc/kubernetes/ssl:/etc/kubernetes/ssl -v /var/etcd:/var/etcd -p 4001:4001 -p 2380:2380 -p 2379:2379 \
  --net=host \
  --restart always \
- --name etcd gcr.io/google-containers/etcd:2.3.7 /usr/local/bin/etcd \
+ --name etcd gcr.io/google-containers/etcd:3.0.4 /usr/local/bin/etcd \
  -name $HOSTNAME \
- -advertise-client-urls https://${HostIP}:2379,https://${HostIP}:4001 \
+ -advertise-client-urls https://{{cnf["etcd_node_ip"]}}:2379,https://{{cnf["etcd_node_ip"]}}:4001 \
  -listen-client-urls https://0.0.0.0:2379,https://0.0.0.0:4001 \
- -initial-advertise-peer-urls https://${HostIP}:2380 \
+ -initial-advertise-peer-urls https://{{cnf["etcd_node_ip"]}}:2380 \
  -listen-peer-urls https://0.0.0.0:2380 \
  -discovery {{cnf["discovery_url"]}} \
  -data-dir /var/etcd/data \
