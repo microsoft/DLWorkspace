d7431e0f58a1f562dab0580b7076c6c21702f612
Hongzhi Li <hongzhi.li@microsoft.com>
2017-01-24 15:47:21 -0800

    use azure sql server as backend database for cluster portal

--- src/ClusterPortal/DataHandler.py
+++ src/ClusterPortal/DataHandler.py
@@ -1,37 +1,58 @@
-import mysql.connector
+import pyodbc
 import json
 import time
 
 class DataHandler:
 	def __init__(self):
-		self.conn = mysql.connector.connect(user="dlworkspace", password="",database="dlworkspace")
-		self.add_cluster_info = ("INSERT INTO cluster_info "
-               "(`clusterId`, `key`, `value`) "
-               "VALUES (%(clusterId)s, %(key)s, %(value)s)")		
 
-		self.add_report = ("INSERT INTO report "
-               "(`hostIP`, `clusterId`, `role`, `clientIP`, `sysId` ) "
-               "VALUES (%(hostIP)s, %(clusterId)s, %(role)s, %(clientIP)s, %(sysId)s)")	
+		self.server = 'tcp:o0ij712tvp.database.windows.net'
+		self.database = 'DLWorkspaceDB'
+		self.username = 'irchallenge'
+		self.password = ''
+		# self.driver = '/usr/lib/x86_64-linux-gnu/libodbc.so'
+		self.driver = '{ODBC Driver 13 for SQL Server}'
+		self.connstr = 'DRIVER='+self.driver+';PORT=1433;SERVER='+self.server+';PORT=1433;DATABASE='+self.database+';UID='+self.username+';PWD='+self.password
+		print "Try to connect with string: " + self.connstr
+		self.conn = pyodbc.connect(self.connstr)
+		print "Connecting to server ..."
+
+
+		self.add_cluster_info = """ 
+		                           INSERT INTO cluster_info 
+					               ([clusterId], [k], [v]) 
+					               VALUES (?,?,?)	
+					            """
+
+		self.add_report = """ insert into report (host_IP,clusterId,role,clientIP,sysId) values  (?, ?, ?, ?, ?) """
 
 	def AddNode(self, data):
 		cursor = self.conn.cursor()
-		cursor.execute(self.add_report, data)
-		id = cursor.lastrowid
+		cursor.execute(self.add_report, data["hostIP"], data["clusterId"], data["role"], data["clientIP"], data["sysId"])
 		self.conn.commit()
 		cursor.close()
 
 
 	def GetNodes(self,role,clusterId):
 		cursor = self.conn.cursor()
-		query = ("SELECT hostIP,clusterId,role, clientIP, time FROM report where role = '"+role+"' and clusterId = '"+clusterId+"' GROUP BY sysId HAVING MAX( TIME ) > ADDTIME( CURRENT_TIMESTAMP ,  '-0:20:0' ) ")
+		query = "SELECT host_IP,clusterId,role, clientIP, time FROM report where role = '"+role+"' and clusterId = '"+clusterId+"' group by sysId "
+		query = """ select a.host_IP,a.clusterId,a.role,a.clientIP, a.time from report a
+					left join report b
+					on a.sysId=b.sysId
+					and a.[time] < b.[time]
+					where b.sysId is NULL 
+					and a.[time] >= DATEADD (hour, -1, getdate())
+					and a.clusterId='%s' 
+					and a.role = '%s'
+					""" % (clusterId,role)
+
 		cursor.execute(query)
 		ret = []
 		for (hostIP,clusterId,role, clientIP, dtime) in cursor:
 			record = {}
-			record["hostIP"] = hostIP
-			record["clusterId"] = clusterId
-			record["role"] = role
-			record["clientIP"] = clientIP
+			record["hostIP"] = hostIP.strip()
+			record["clusterId"] = clusterId.strip()
+			record["role"] = role.strip()
+			record["clientIP"] = clientIP.strip()
 			record["time"] = time.mktime(dtime.timetuple())
 		  	ret.append(record)
 		cursor.close()
@@ -40,19 +61,18 @@ class DataHandler:
 
 	def AddClusterInfo(self, data):
 		cursor = self.conn.cursor()
-		cursor.execute(self.add_cluster_info, data)
-		id = cursor.lastrowid
+		cursor.execute(self.add_cluster_info, data["clusterId"], data["key"], data["value"])
 		self.conn.commit()
 		cursor.close()
 
 
 	def GetClusterInfo(self,clusterId,key):
 		cursor = self.conn.cursor()
-		query = ("SELECT `key`, `value` FROM cluster_info where `key` = '"+key+"' and `clusterId` = '"+clusterId+"' order by time desc limit 1")
+		query = ("SELECT TOP 1 [k], [v] FROM cluster_info where [k] = '"+key+"' and [clusterId] = '"+clusterId+"' order by [time] desc")
 		cursor.execute(query)
 		value = None
 		for (k,v) in cursor:
-			value = v
+			value = v.strip()
 		cursor.close()
 		return value		
 	
@@ -62,25 +82,36 @@ class DataHandler:
 		query = ("DELETE FROM cluster_info where clusterId = '"+clusterId+"'")
 		cursor.execute(query)
 		cursor.close()
+		self.conn.commit()
 
 
 	def Close(self):
 		self.conn.close()
 
 if __name__ == '__main__':
-	TEST_INSERT_JOB = False
-	TEST_QUERY_JOB_LIST = True
+	TEST_INSERT_NODE = False
+	TEST_GET_NODE = False
+	TEST_INSTER_CLUSTER_INFO = True
 	dataHandler = DataHandler()
 	
-	if TEST_INSERT_JOB:
+	if TEST_INSERT_NODE:
 		jobParams = {}
-		jobParams["id"] = "dist-tf-00001"
-		jobParams["job-name"] = "dist-tf"
-		jobParams["user-id"] = "hongzl"
-		jobParams["job-meta-path"] = "/dlws/jobfiles/***"
-		jobParams["job-meta"] = "ADSCASDcAE!EDASCASDFD"
+		jobParams["hostIP"] = "10.10.10.10"
+		jobParams["clusterId"] = "1234556"
+		jobParams["role"] = "worker"
+		jobParams["clientIP"] =  "10.10.10.10"
+		jobParams["sysId"] = "ADSCASDcAE!EDASCASDFD"
 		
-		dataHandler.AddJob(jobParams)
-	
-	if TEST_QUERY_JOB_LIST:
-		print dataHandler.GetJobList()
+		dataHandler.AddNode(jobParams)
+	if TEST_GET_NODE:
+		print dataHandler.GetNodes("worker","1234556")
+
+	if TEST_INSTER_CLUSTER_INFO:
+		data = {}
+		data["clusterId"] = "1234"
+		data["key"] = "key"
+		data["value"] = "value"
+		dataHandler.AddClusterInfo(data)
+
+	print dataHandler.GetClusterInfo("1234","key")
+	dataHandler.DeleteCluster("1234")
