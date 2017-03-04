bd7a46d2d34749e1463c0733d3bdb4e73f8b2376
Jin Li <jinlmsft@hotmail.com>
2017-03-03 19:01:42 -0800

    Make ETCD port configurable

--- src/ClusterBootstrap/template/etcd/docker_etcd_ssl.sh
+++ src/ClusterBootstrap/template/etcd/docker_etcd_ssl.sh
@@ -4,15 +4,15 @@
 docker rm -f philly-etcd3
 sudo rm -r /var/etcd/data
 
-docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -v /etc/etcd/ssl:/etc/etcd/ssl -v /var/etcd:/var/etcd -p 4001:4001 -p 2380:2380 -p 2379:2379 \
+docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -v /etc/etcd/ssl:/etc/etcd/ssl -v /var/etcd:/var/etcd -p {{cnf["etcd3port1"]}}:{{cnf["etcd3port1"]}} -p {{cnf["etcd3portserver"]}}:{{cnf["etcd3portserver"]}} -p {{cnf["etcd3port2"]}}:{{cnf["etcd3port2"]}} \
  --net=host \
  --restart always \
  --name philly-etcd3 gcr.io/google-containers/etcd:3.0.4 /usr/local/bin/etcd \
  -name $HOSTNAME \
- -advertise-client-urls https://{{cnf["etcd_node_ip"]}}:2379,https://{{cnf["etcd_node_ip"]}}:4001 \
- -listen-client-urls https://0.0.0.0:2379,https://0.0.0.0:4001 \
- -initial-advertise-peer-urls https://{{cnf["etcd_node_ip"]}}:2380 \
- -listen-peer-urls https://0.0.0.0:2380 \
+ -advertise-client-urls https://{{cnf["etcd_node_ip"]}}:{{cnf["etcd3port1"]}},https://{{cnf["etcd_node_ip"]}}:{{cnf["etcd3port2"]}} \
+ -listen-client-urls https://0.0.0.0:{{cnf["etcd3port1"]}},https://0.0.0.0:{{cnf["etcd3port2"]}} \
+ -initial-advertise-peer-urls https://{{cnf["etcd_node_ip"]}}:{{cnf["etcd3portserver"]}} \
+ -listen-peer-urls https://0.0.0.0:{{cnf["etcd3portserver"]}} \
  -discovery {{cnf["discovery_url"]}} \
  -data-dir /var/etcd/data \
  -client-cert-auth \
