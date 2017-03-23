8bb4ea2b2dfc981f37efb4a19be0daab4bbc5b13
Hongzhi Li <hongzhi.li@microsoft.com>
2017-03-23 10:39:28 -0700

    use apache/wsgi to run restfulapi

---
+++ src/docker-images/RestfulAPI/dlws-restfulapi.wsgi
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0, '/DLWorkspace/src/RestAPI')
+from dlwsrestapi import app as application
--- src/docker-images/RestfulAPI/run.sh
+++ src/docker-images/RestfulAPI/run.sh
@@ -3,5 +3,5 @@ ssh-keyscan github.com >> /root/.ssh/known_hosts
 rm -r /DLWorkspace
 ssh-agent bash -c 'ssh-add /root/.ssh/id_rsa; git clone -b webUI git@github.com:MSRCCS/DLWorkspace.git /DLWorkspace'
 cp /RestfulAPI/config.yaml /DLWorkspace/src/utils/config.yaml
-
-python /DLWorkspace/src/RestAPI/dlws-restapi.py
+#python /DLWorkspace/src/RestAPI/dlws-restapi.py
+/usr/sbin/apache2ctl -D FOREGROUND
