36f90182f9bcd059f814639fdeee60744f92a45e
Jin Li <jinlmsft@hotmail.com>
2017-04-05 19:31:47 -0700

    Glusterfs: logging to both docker console and file for monitoring of activity during setup.

--- src/docker-images/glusterfs/launch_glusterfs.py
+++ src/docker-images/glusterfs/launch_glusterfs.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python 
 # launch gluster fs 
 import time
+import os
 from datetime import datetime
+import yaml
+import logging
+import logging.config
 
-curtime = datetime.utcnow()
-# set up logging to file - see previous section for more details
-print "Start glusterfs ...." 
+def create_log( logdir ):
+	if not os.path.exists( logdir ):
+		os.system("mkdir -p " + logdir )
+	curtime = datetime.utcnow()
+	fname = os.path.join( logdir, str(curtime) + ".log" )
+	
+def start_glusterfs( logdir = '/var/log/glusterfs/launch' ):
+	with open('logging.yaml') as f:
+		logging_config = yaml.load(f)
+		f.close()
+		logging.config.dictConfig(logging_config)
+	create_log( logdir )
+	# set up logging to file - see previous section for more details
+	logging.debug ("Start glusterfs ...." )
 
 
 
 
 if __name__ == '__main__':
+	start_glusterfs()
 	while True:
-		print "Keep container alive, time ... " + str(datetime.utcnow()) 
+		logging.debug( "Keep container alive, time ... " + str(datetime.utcnow()) )
 		time.sleep(5)
 
