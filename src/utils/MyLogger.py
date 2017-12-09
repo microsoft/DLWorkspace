0d0401bff2c6d3f079389b50d2bebc0c6366f6a7
Hongzhi Li <hongzhi.li@microsoft.com>
2017-12-08 17:18:02 -0800

    support mysql as datasource

--- src/utils/MyLogger.py
+++ src/utils/MyLogger.py
@@ -14,30 +14,34 @@ class MyLogger:
 
     def info(self,msg):
         self.init()
-
+        print msg
         if self.logger is not None:
             self.logger.info(msg)
 
     def info(self,msg):
         self.init()
+        print msg
 
         if self.logger is not None:
             self.logger.info(msg)
 
     def error(self,msg):
         self.init()
+        print msg
 
         if self.logger is not None:
             self.logger.error(msg)
 
     def warn(self,msg):
         self.init()
+        print msg
 
         if self.logger is not None:
             self.logger.warn(msg)
 
     def debug(self,msg):
         self.init()
+        print msg
 
         if self.logger is not None:
             self.logger.debug(msg)
