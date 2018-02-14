b90bf5b52efa3455abc1ef7ba7e7651e4cab160f
Hongzhi Li <hongzhi.li@microsoft.com>
2018-02-14 09:57:38 -0800

    generate authorized key file for all users

--- src/ClusterManager/user_manager.py
+++ src/ClusterManager/user_manager.py
@@ -73,10 +73,12 @@ def set_user_directory():
             os.system("chown -R "+userid+":"+"500000513 "+userpath)
             os.system("chmod 700 -R "+os.path.dirname(sshkeypath))
 
+
+        if not os.path.exists(authorized_keyspath):
+            logging.info("Creating authorized_keys for user %s" % (username))
+            os.system("chown -R "+userid+":"+"500000513 "+authorized_keyspath)
             os.system("cat "+pubkeypath+" >> "+authorized_keyspath)
             os.system("chmod 644 "+authorized_keyspath)
-
-
 
 def Run():
     create_log()
