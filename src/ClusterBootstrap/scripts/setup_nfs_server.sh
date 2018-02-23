31ab765bd587617d84b5cc14bacf2ca14c028687
Jin Li <jinlmsft@hotmail.com>
2018-02-22 17:27:32 -0800

    On squash root for NFS server.

--- src/ClusterBootstrap/scripts/setup_nfs_server.sh
+++ src/ClusterBootstrap/scripts/setup_nfs_server.sh
@@ -4,7 +4,7 @@ sudo apt-get install nfs-kernel-server
 sudo mkdir -p /mnt/share 
 sudo chown nobody:nogroup /mnt/share
 
-echo "/mnt/share {{cnf["cloud_config"]["vnet_range"]}}(rw,sync,no_subtree_check)" | sudo tee /etc/exports
+echo "/mnt/share {{cnf["cloud_config"]["vnet_range"]}}(rw,sync,no_subtree_check,no_root_squash)" | sudo tee /etc/exports
 sudo systemctl restart nfs-kernel-server
 
 
