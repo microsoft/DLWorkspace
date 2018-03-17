b0892b7458f8bf7f8ba172f8a52594a8e4cf4151
Jin Li <jinlmsft@hotmail.com>
2018-03-16 18:02:07 -0700

    Customize recognizer.

--- src/docker-images/tutorial-tensorflow/RecogServer/RecogServer/Controllers/RecogController.cs
+++ src/docker-images/tutorial-tensorflow/RecogServer/RecogServer/Controllers/RecogController.cs
@@ -99,5 +99,83 @@ namespace RecogServer.Controllers
                 return Ok(new { error = ex.ToString(), result = ret });
             }
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CustomRecog()
+        {
+            var forms = Request.Form;
+            var postdata = new JObject();
+            foreach (var key in forms.Keys)
+            {
+                forms.TryGetValue(key, out StringValues values);
+                if (!String.IsNullOrEmpty(values[0]))
+                {
+                    postdata[key] = values[0];
+                }
+            }
+            // _logger.LogInformation($"Recog image: {postdata}");
+
+            Int64 totalUpload = 0L;
+            var files = Request.Form.Files;
+            JObject ret = new JObject();
+
+            try
+            {
+
+                foreach (var file in files)
+                {
+                    if (file.Length > 0)
+                    {
+                        var filename = file.Name;
+                        var ext = Path.GetExtension(filename);
+                        var imagename = Guid.NewGuid().ToString() + ext;
+                        using (var stream = new FileStream(imagename, FileMode.Create))
+                        {
+                            await file.CopyToAsync(stream);
+                            totalUpload += stream.Length;
+                            var logInfo = new { filename = filename, size = stream.Length };
+                            _logger.LogInformation($"CustomRecog: {logInfo}");
+                            var recogprog = "/tensorflow/tensorflow/examples/label_image/label_image.py";
+                            var customdir = "/custom/model";
+                            foreach( )
+                            var recoggraph = "/tensorflow/tensorflow/examples/label_image/data/inception_v3_2016_08_28_frozen.pb";
+                            var label = "/tensorflow/tensorflow/examples/label_image/data/imagenet_slim_labels.txt";
+
+                            var command = $"{recogprog} --graph {recoggraph} --image {imagename} --labels {label}";
+                            int nWait = Interlocked.Increment(ref RecogController.nWait);
+                            var thisResult = new JObject();
+                            if (nWait < RecogController.maxWait)
+                            {
+                                try
+                                {
+                                    await sem.WaitAsync();
+                                    var tuple = await ProcessUtils.RunProcessAsync("/usr/bin/python3", command);
+                                    sem.Release();
+                                    thisResult["code"] = tuple.Item1;
+                                    thisResult["output"] = tuple.Item2;
+                                    thisResult["error"] = tuple.Item3;
+                                }
+                                catch (Exception)
+                                { }
+                            }
+                            else
+                            {
+                                thisResult["error"] = $"Too many recognition items: {nWait}, not waiting. ";
+                            }
+                            Interlocked.Decrement(ref RecogController.nWait);
+
+                            ret[filename] = thisResult;
+                        }
+                    }
+                }
+                return Ok(new { result = ret });
+            }
+            catch (Exception ex)
+            {
+                var errorLog = new { exception = ex.ToString() };
+                _logger.LogInformation("ReogImage: {0}", errorLog);
+                return Ok(new { error = ex.ToString(), result = ret });
+            }
+        }
     }
 }
