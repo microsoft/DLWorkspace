6082c62d6b4cfd07520e281b8f8f7187af66a896
Jin Li <jinlmsft@hotmail.com>
2018-12-30 14:47:59 -0800

    install nvidia-docker2

--- src/ClusterBootstrap/scripts/remove_nvidia_docker1.sh
+++ src/ClusterBootstrap/scripts/remove_nvidia_docker1.sh
@@ -1,2 +1,11 @@
 docker volume ls -q -f driver=nvidia-docker | xargs -r -I{} -n1 docker ps -q -a -f volume={} | xargs -r docker rm -f
 sudo apt-get purge nvidia-docker
+
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+
+sudo apt-get install nvidia-docker2
+sudo pkill -SIGHUP dockerd
