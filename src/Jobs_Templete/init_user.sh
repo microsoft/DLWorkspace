c89b40ae866aa35ed7ea41cd363d92bf51eed0c3
Hao Yuan <hao1939@gmail.com>
2019-05-23 15:36:46 +0800

    refine the init_user.sh

--- src/Jobs_Templete/init_user.sh
+++ src/Jobs_Templete/init_user.sh
@@ -1,31 +1,35 @@
 #/bin/bash
 set -ex
 
-#export POD_NAME=0511068a-7744-47af-b42f-bd3695
-#export DLWS_GID=234324
-#export DLWS_UID=234324
-#export DLWS_USER=hayua
-export DLWS_DIR=/dlws
+#export POD_NAME=
+#export DLWS_GID=
+#export DLWS_UID=
+#export DLWS_USER_NAME=
+export ENV_FILE=/dlws/pod.env
 
 # setup user and group, fix permissions
 addgroup --force-badname --gid  ${DLWS_GID} domainusers
-adduser --force-badname --home /home/${DLWS_USER} --shell /bin/bash --uid ${DLWS_UID}  -gecos '' --gid ${DLWS_GID} --disabled-password ${DLWS_USER}
-usermod -p $(echo tryme2017 | openssl passwd -1 -stdin) ${DLWS_USER}
-chown -R ${DLWS_USER} /home/${DLWS_USER}/ || /bin/true
+adduser --force-badname --home /home/${DLWS_USER_NAME} --shell /bin/bash --uid ${DLWS_UID}  -gecos '' --gid ${DLWS_GID} --disabled-password ${DLWS_USER_NAME}
+usermod -p $(echo tryme2017 | openssl passwd -1 -stdin) ${DLWS_USER_NAME}
+chown -R ${DLWS_USER_NAME} /home/${DLWS_USER_NAME}/ || /bin/true
 
 # setup sudoers
-adduser $DLWS_USER sudo
+adduser $DLWS_USER_NAME sudo
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 
 # export envs
 env | while read line; do
-        if [[ $line != HOME=* ]] && [[ $line != INTERACTIVE* ]] && [[ $line != LS_COLORS* ]]  && [[ $line != PATH* ]]; then
-            echo "export $line" >> ${DLWS_DIR}/${POD_NAME}.env ;
+        if [[ $line != HOME=* ]] && [[ $line != INTERACTIVE* ]] && [[ $line != LS_COLORS* ]]  && [[ $line != PATH* ]] && [[ $line != PWD* ]]; then
+            echo "export $line" >> "${ENV_FILE}" ;
         fi; done
-echo "export PATH=$PATH:\${PATH}" >> ${DLWS_DIR}/${POD_NAME}.env
-echo "export LD_LIBRARY_PATH=/usr/local/nvidia/lib64/:\${LD_LIBRARY_PATH}" >> ${DLWS_DIR}/${POD_NAME}.env
+echo "export PATH=$PATH:\${PATH}" >> "${ENV_FILE}"
+echo "export LD_LIBRARY_PATH=/usr/local/nvidia/lib64/:\${LD_LIBRARY_PATH}" >> "${ENV_FILE}"
 # source the envs
-echo "source ${DLWS_DIR}/${POD_NAME}.env" >> /home/${DLWS_USER}/.profile
+grep -qx "^\s*. ${ENV_FILE}" /home/${DLWS_USER_NAME}/.profile || cat << SCRIPT >> "/home/${DLWS_USER_NAME}/.profile"
+if [ -f ${ENV_FILE} ]; then
+    . ${ENV_FILE}
+fi
+SCRIPT
 
-# any command should run as ${DLWS_USER}
-#runuser -l ${DLWS_USER} -c your_commands
+# any command should run as ${DLWS_USER_NAME}
+#runuser -l ${DLWS_USER_NAME} -c your_commands
