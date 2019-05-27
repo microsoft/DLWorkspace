d1568a94687dcc006a52e8a4a7430770709d45d7
Hao Yuan <hao1939@gmail.com>
2019-05-28 01:12:21 +0800

    fix hostNetwork ssh server port

--- src/ClusterManager/job_manager.py
+++ src/ClusterManager/job_manager.py
@@ -655,9 +655,10 @@ def start_ssh_server(pod_name, user_name, ssh_port=22, host_network=False):
     # ssh_port = 22
 
     # modify the script for HostNewtork
-    # if the ssh_port is default value 22, randomly choose one
-    if host_network and ssh_port==22:
-        ssh_port = random.randint(40001, 49999)
+    if host_network:
+        # if the ssh_port is default value 22, randomly choose one
+        if ssh_port== 22:
+            ssh_port = random.randint(40001, 49999)
         # bash_script = "sed -i '/^Port 22/c Port "+str(ssh_port)+"' /etc/ssh/sshd_config && "+bash_script
         # TODO refine the script later
         bash_script = "sudo bash -c 'apt-get update && apt-get install -y openssh-server && sed -i \"s/^Port 22/c Port " + str(ssh_port) + "/\" /etc/ssh/sshd_config && cd /home/" + user_name + " && mkdir -p ssh && chmod 700 ssh && cat .ssh/id_rsa.pub >> ssh/authorized_keys && chmod 600 ssh/authorized_keys && sed -i \"s/^[#]*AuthorizedKeysFile.*/AuthorizedKeysFile      %h\/ssh\/authorized_keys/\" /etc/ssh/sshd_config && service ssh restart'"
