ceff2067ecf5595f4d5ef36a851c84d8adbcd935
Di Xu <xudifsd@gmail.com>
2019-06-17 08:18:30 +0000

    support distributed jobs

--- src/ClusterManager/node_manager.py
+++ src/ClusterManager/node_manager.py
@@ -67,7 +67,7 @@ def get_job_gpu_usage(jobId):
     try:
         hostaddress = config.get("prometheus_node", "127.0.0.1")
 
-        url = """http://"""+hostaddress+""":9091/prometheus/api/v1/query?query=avg%28avg_over_time%28task_gpu_percent%7Bjob_name%3D%22""" + jobId + """%22%7D%5B4h%5D%29%29+by+%28job_name%2C+instance%2C+username%29"""
+        url = """http://"""+hostaddress+""":9091/prometheus/api/v1/query?query=avg%28avg_over_time%28task_gpu_percent%7Bpod_name%3D%22""" + jobId + """%22%7D%5B4h%5D%29%29+by+%28pod_name%2C+instance%2C+username%29"""
 
         curl = pycurl.Curl()
         curl.setopt(pycurl.URL, url)
