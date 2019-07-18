a559c478ee71a6a583b2645a0e1b92545eec57cb
Hao Yuan <hao1939@gmail.com>
2019-07-18 04:47:35 +0000

    reformat

--- src/ClusterManager/cluster_manager.py
+++ src/ClusterManager/cluster_manager.py
@@ -8,12 +8,13 @@ import time
 
 logger = logging.getLogger(__name__)
 
+
 def create_log(logdir="/var/log/dlworkspace"):
     if not os.path.exists(logdir):
         os.system("mkdir -p " + logdir)
     with open("logging.yaml") as f:
         logging_config = yaml.load(f)
-    logging_config["handlers"]["file"]["filename"] = logdir+"/clustermanager.log"
+    logging_config["handlers"]["file"]["filename"] = logdir + "/clustermanager.log"
     logging.config.dictConfig(logging_config)
 
 
@@ -22,13 +23,13 @@ def Run():
 
     cwd = os.path.dirname(__file__)
     cmds = [
-            ["python", os.path.join(cwd, "job_manager.py")],
-            ["python", os.path.join(cwd, "user_manager.py")],
-            ["python", os.path.join(cwd, "node_manager.py")],
-            ["python", os.path.join(cwd, "joblog_manager.py")],
-            ["python", os.path.join(cwd, "command_manager.py")],
-            ["python", os.path.join(cwd, "endpoint_manager.py")],
-            ]
+        ["python", os.path.join(cwd, "job_manager.py")],
+        ["python", os.path.join(cwd, "user_manager.py")],
+        ["python", os.path.join(cwd, "node_manager.py")],
+        ["python", os.path.join(cwd, "joblog_manager.py")],
+        ["python", os.path.join(cwd, "command_manager.py")],
+        ["python", os.path.join(cwd, "endpoint_manager.py")],
+    ]
 
     FNULL = open(os.devnull, "w")
 
@@ -41,12 +42,11 @@ def Run():
                 if child is not None:
                     logger.info("%s is dead restart it", cmd)
                 try:
-                    childs[i] = subprocess32.Popen(cmd,
-                            stdin=FNULL, close_fds=True)
+                    childs[i] = subprocess32.Popen(cmd, stdin=FNULL, close_fds=True)
                 except Exception as e:
-                    logger.exception("caught exception when trying to start %s, ignore",
-                            cmd)
+                    logger.exception("caught exception when trying to start %s, ignore", cmd)
         time.sleep(60)
+
 
 if __name__ == "__main__":
     sys.exit(Run())
