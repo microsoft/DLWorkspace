a295f4ff8220e4a78db2aafc5cade612de717bb8
Di Xu <xudifsd@gmail.com>
2019-08-14 05:09:03 +0000

    upgrade nvidia driver

--- src/ClusterBootstrap/scripts/prepare_ubuntu.sh
+++ src/ClusterBootstrap/scripts/prepare_ubuntu.sh
@@ -79,12 +79,35 @@ if  lspci | grep -qE "[0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F].[0-9] (3D|VG
  #       chmod +x /tmp/NVIDIA-Linux-x86_64-$NVIDIA_VERSION.run
  #       sudo bash /tmp/NVIDIA-Linux-x86_64-$NVIDIA_VERSION.run -a -s
 
-    sudo add-apt-repository -y ppa:graphics-drivers/ppa
-	sudo apt-get purge -y nvidia*
-    sudo apt-get update
-	sudo apt-get install -y nvidia-driver-415
-	
+    sudo systemctl stop kubelet
 
+    echo kill all containers so we could remove old nvidia drivers
+    timeout 10 docker kill $(docker ps -a -q)
+
+    lsmod | grep -qE "^nvidia" &&
+        {
+            echo ======== NVIDIA driver is running, uninstall it =========
+            DEP_MODS=`lsmod | tr -s " " | grep -E "^nvidia" | cut -f 4 -d " "`
+            for mod in ${DEP_MODS//,/ }
+            do
+                sudo rmmod $mod ||
+                    {
+                        echo "The driver $mod is still in use, can't unload it."
+                        exit 1
+                    }
+            done
+            sudo rmmod nvidia ||
+                {
+                    echo "The driver nvidia is still in use, can't unload it."
+                    exit 1
+                }
+        }
+
+    sudo add-apt-repository -y ppa:graphics-drivers/ppa
+
+    sudo apt-get purge -y nvidia*
+    sudo apt-get update
+    sudo apt-get install -y nvidia-driver-430
 
         sudo apt install -y nvidia-modprobe
 
