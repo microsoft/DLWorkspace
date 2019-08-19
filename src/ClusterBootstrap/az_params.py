47bbcb6d3c08ec2c05d6326ceb42e0cd2571c777
Zhe <zhe_ms@zhe_ms_blv.guest.corp.microsoft.com>
2019-08-19 21:52:57 +0000

    update default vm image version used to create cluster

--- src/ClusterBootstrap/az_params.py
+++ src/ClusterBootstrap/az_params.py
@@ -5,7 +5,7 @@ default_az_parameters = {
         "azure_location": "westus2",
         "infra_vm_size" : "Standard_D1_v2",
         "worker_vm_size": "Standard_NC6",
-        "vm_image" : "UbuntuLTS",
+        "vm_image" : "Canonical:UbuntuServer:18.04-LTS:18.04.201907221",
         "vm_storage_sku" : "Premium_LRS",        
         # "udp_port_ranges": ""
         # Use file_share_name to create Azure file share
