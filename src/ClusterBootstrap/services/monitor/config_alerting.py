de09fb2230a2cc3bfee47e6987ddca937b163830
Di Xu <xudifsd@gmail.com>
2019-08-30 07:16:49 +0000

    support config kill through config.yaml

---
+++ src/ClusterBootstrap/services/monitor/config_alerting.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import yaml
+
+headers = """
+groups:
+  - name: kill-idle
+    rules:
+"""
+
+kill_template = """
+    - alert: kill-idle-jobs-email-%s
+      for: %dh
+      expr: avg(task_gpu_percent{vc_name="%s"}) by (user_email, job_name, vc_name) == 0
+      labels:
+        type: kill_idle_job_email
+    - alert: kill-idle-jobs-%s
+      for: %dh
+      expr: avg(task_gpu_percent{vc_name="%s"}) by (user_email, job_name, vc_name) == 0
+      labels:
+        type: reaper
+"""
+
+def config_kill_rule(m):
+    for vc_name, hour in m.items():
+        print(kill_template % (vc_name, hour, vc_name, vc_name, hour, vc_name))
+
+def extract_relevant_config(config_map):
+    return config_map.get("prometheus", {}).get("alerting", {}).get("kill-idle", {})
+
+if __name__ == "__main__":
+    with open(sys.argv[1]) as f:
+        config = yaml.load(f.read())
+
+    print(headers)
+    config_kill_rule(extract_relevant_config(config))
