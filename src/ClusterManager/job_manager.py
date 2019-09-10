639b15f3584aeddb708498f65eb387f30861a618
Hao Yuan <hao1939@gmail.com>
2019-09-10 03:54:58 +0000

    log more info about pod

--- src/ClusterManager/job_manager.py
+++ src/ClusterManager/job_manager.py
@@ -314,6 +314,11 @@ def check_job_status(job_id):
     statuses = [job_role.status() for job_role in job_roles]
     logging.info("Job: {}, status: {}".format(job_id, statuses))
 
+    details = []
+    for job_role in job_roles:
+        details.append(job_role.pod_details().to_dict())
+    logging.info("Job {}, details: {}".format(job_id, details))
+
     if "Failed" in statuses:
         return "Failed"
     if "Unknown" in statuses:
