a867f1fd7e564414f592805e4a1d4f0b56011dcf
hongyiliu <hongyiliu1992@gmail.com>
2019-09-25 19:24:16 -0700

    Change tabs of every page

--- src/dashboard/src/utlities/layoutUtlities.ts
+++ src/dashboard/src/utlities/layoutUtlities.ts
@@ -2,7 +2,7 @@ import {useMediaQuery, useTheme} from "@material-ui/core";
 
 const useCheckIsDesktop = () => {
   const theme = useTheme();
-  return useMediaQuery(theme.breakpoints.up("sm"));
+  return useMediaQuery(theme.breakpoints.up("md"));
 }
 
 export default useCheckIsDesktop
