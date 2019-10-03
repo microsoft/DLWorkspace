f8199b46330b6aed79a08e889ec68e80772d92dd
hongyiliu <hongyiliu1992@gmail.com>
2019-10-02 19:03:41 -0700

    Change the totatl display workflow and fix issue of jobs for owner

--- src/dashboard/src/pages/ClusterStatus/components/TeamVCUserStatus.tsx
+++ src/dashboard/src/pages/ClusterStatus/components/TeamVCUserStatus.tsx
@@ -26,7 +26,7 @@ export const TeamVCUserStatus = (props: TeamUsr) => {
             {title: 'Currently Idle GPU', field: 'idleGPU',type:'numeric'},
             {title: 'Past Month Booked GPU Hour', field: 'booked',type:'numeric'},
             {title: 'Past Month Idle GPU Hour', field: 'idle',type:'numeric'},
-            {title: 'Past Month Idle GPU Hour %', field: '',type:'numeric', render: (rowData: any) => <span style={{ color: Math.floor((rowData['idle'] / rowData['booked']) * 100) > 50 ? "red" : "black" }}>{rowData['booked'] == '0' ? '-' : Math.floor((rowData['idle'] / rowData['booked']) * 100)}</span>, customSort: (a: any, b: any) => {return Math.floor((a['idle'] / a['booked']) * 100) - Math.floor((b['idle'] / b['booked']) * 100)}}]} data={showCurrentUser ? userStatus.filter((uc: any)=>uc['usedGPU'] > 0) : userStatus}
+            {title: 'Past Month Idle GPU Hour %', field: '',type:'numeric', render: (rowData: any) => <span style={{ color: Math.floor((rowData['idle'] / rowData['booked']) * 100) > 50 ? "red" : "black" }}>{rowData['booked'] == '0' ? '-' : Math.floor((rowData['idle'] / rowData['booked']) * 100)}</span>, customSort: (a: any, b: any) => {return Math.floor((a['idle'] / a['booked']) * 100) - Math.floor((b['idle'] / b['booked']) * 100)}}]} data={showCurrentUser ? userStatus.filter((uc: any)=>uc['usedGPU'] > 0 && uc['userName'] !== 'Total') : userStatus}
           options={{filtering: false,paging: false,sorting: true}}
           components={{
             Toolbar: props => (
