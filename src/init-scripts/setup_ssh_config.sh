b7c42763f59868229e3a283f167f76de9d4ca132
REDMOND\anbhu <anbhu@microsoft.com>
2019-10-04 15:02:17 -0700

    Order hosts by index in config file

--- src/init-scripts/setup_ssh_config.sh
+++ src/init-scripts/setup_ssh_config.sh
@@ -6,14 +6,14 @@ JOB_DIR='/job'
 
 if [ "$DLWS_ROLE_NAME" = "ps" ];
 then
-    # wait untill all workers are ready
+    # wait until all workers are ready
     all_workers_ready=false
     while [ "$all_workers_ready" != true ]
     do
-        # update it to false if any woker is not ready
+        # update it to false if any worker is not ready
         all_workers_ready=true
 
-        for i in $(seq 0 $(( ${DLWS_WORKER_NUM} - 1)) )
+        for i in $(seq 0 $(( ${DLWS_NUM_WORKER} - 1 )))
         do
             worker="worker-${i}"
             file="${JOB_DIR}/${worker}/running/ROLE_READY"
@@ -28,18 +28,31 @@ then
     done
 fi
 
+# generate ps host list
+ps_host_list=""
+for i in $(seq 0 $(( ${DLWS_NUM_PS} - 1 )) )
+do
+    ps_host_list+="ps-${i} "
+done
+
+# generate worker host list
+worker_host_list=""
+for i in $(seq 0 $(( ${DLWS_NUM_WORKER} - 1 )) )
+do
+    worker_host_list+="worker-${i} "
+done
+
+# generate host list
+host_list="${ps_host_list} ${worker_host_list}"
+
 # generate ~/ssh_config
 SSH_CONFIG_FILE="/job/ssh_config"
 >${SSH_CONFIG_FILE}
 chown ${DLWS_USER_NAME} ${SSH_CONFIG_FILE}
-for role_dir in ${JOB_DIR}/*/ # list directories in the form "/JOB_DIR/role/"
+
+for host in ${host_list}
 do
-    role_dir=${role_dir%*/} # remove the trailing "/"
-    if [[ $role_dir == *logs ]];
-    then
-        continue
-    fi
-    host=$(basename ${role_dir})
+    role_dir=${JOB_DIR}/${host}
     port=$(cat "${role_dir}/running/SSH_PORT")
     ip=$(cat "${role_dir}/running/POD_IP")
     cat >>${SSH_CONFIG_FILE} <<EOF
@@ -52,7 +65,6 @@ Host ${host}
   UserKnownHostsFile /dev/null
 
 EOF
-
 done
 
 # copy ssh config to ~/.ssh/config
@@ -74,17 +86,11 @@ done
 SLOT_FILE="/job/hostfile"
 >${SLOT_FILE}
 chown ${DLWS_USER_NAME} ${SLOT_FILE}
-for role_dir in ${JOB_DIR}/*/ # list directories in the form "/JOB_DIR/role/"
+
+for host in ${worker_host_list}
 do
-    role_dir=${role_dir%*/} # remove the trailing "/"
-    if [[ $role_dir == *logs ]] || [[ $role_dir == *ps* ]];
-    then
-        continue
-    fi
-    host=$(basename ${role_dir})
     slots=${DLWS_NUM_GPU_PER_WORKER}
     cat >>${SLOT_FILE} <<EOF
 ${host} slots=${slots}
 EOF
-
 done
