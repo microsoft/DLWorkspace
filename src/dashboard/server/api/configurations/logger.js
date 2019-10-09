16d3d009836997420ded1dbc7f60832c6e7d4284
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2019-10-09 17:46:31 +0800

    Disable logger in test env

--- src/dashboard/server/api/configurations/logger.js
+++ src/dashboard/server/api/configurations/logger.js
@@ -5,8 +5,9 @@ const logger = require('koa-pino-logger')
  * @param {import('koa')} app
  */
 module.exports = (app) => {
+  const enabled = app.env !== 'test'
   const prettyPrint = app.env === 'development'
-  const middleware = logger({ prettyPrint })
+  const middleware = logger({ enabled, prettyPrint })
 
   // Log the config
   middleware.logger.info(config.util.toObject(), 'App config')
