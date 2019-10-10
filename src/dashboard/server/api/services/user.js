2e1681d21815df06d93e84c98d112438e1c0168c
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2019-10-10 17:41:38 +0800

    Externalize User.generateToken for test use

--- src/dashboard/server/api/services/user.js
+++ src/dashboard/server/api/services/user.js
@@ -40,7 +40,8 @@ class User extends Service {
 
   /**
    * @param {import('koa').Context} context
-   * @param {object} idToken
+   * @param {string} email
+   * @param {string} token
    * @return {User}
    */
   static fromToken (context, email, token) {
@@ -69,11 +70,19 @@ class User extends Service {
     return user
   }
 
+  /**
+   * @param {string} email
+   * @return {Buffer}
+   */
+  static generateToken (email) {
+    const hash = createHash('md5')
+    hash.update(`${email}:${masterToken}`)
+    return hash.digest()
+  }
+
   get token () {
     if (this._token == null) {
-      const hash = createHash('md5')
-      hash.update(`${this.email}:${masterToken}`)
-      this._token = hash.digest()
+      this._token = User.generateToken(this.email)
     }
     return this._token
   }
