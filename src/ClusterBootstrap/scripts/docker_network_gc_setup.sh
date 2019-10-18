7b8813d27a163309969a828cb8aa85b35b827ac8
REDMOND\anbhu <anbhu@microsoft.com>
2019-10-18 15:24:35 -0700

    Double DNS threshold3 for arp

--- src/ClusterBootstrap/scripts/docker_network_gc_setup.sh
+++ src/ClusterBootstrap/scripts/docker_network_gc_setup.sh
@@ -9,7 +9,7 @@ grep -xF "net.ipv4.neigh.default.gc_interval = 3600" /tmp/sysctl.conf || echo "n
 grep -xF "net.ipv4.neigh.default.gc_stale_time = 3600" /tmp/sysctl.conf || echo "net.ipv4.neigh.default.gc_stale_time = 3600" | sudo tee -a  /tmp/sysctl.conf
 
 # Setup DNS threshold for arp
-grep -xF "net.ipv4.neigh.default.gc_thresh3 = 4096" /tmp/sysctl.conf || echo "net.ipv4.neigh.default.gc_thresh3 = 4096" | sudo tee -a /tmp/sysctl.conf
+grep -xF "net.ipv4.neigh.default.gc_thresh3 = 8192" /tmp/sysctl.conf || echo "net.ipv4.neigh.default.gc_thresh3 = 4096" | sudo tee -a /tmp/sysctl.conf
 grep -xF "net.ipv4.neigh.default.gc_thresh2 = 2048" /tmp/sysctl.conf || echo "net.ipv4.neigh.default.gc_thresh2 = 2048" | sudo tee -a /tmp/sysctl.conf
 grep -xF "net.ipv4.neigh.default.gc_thresh1 = 1024" /tmp/sysctl.conf || echo "net.ipv4.neigh.default.gc_thresh1 = 1024" | sudo tee -a /tmp/sysctl.conf
 
