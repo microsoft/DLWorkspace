c321ca913e2d95d9a4fa484de3874220acac758e
hongyiliu <hongyiliu1992@gmail.com>
2019-10-21 16:59:56 -0700

    Change the layout of Piechart

--- src/dashboard/src/pages/Home/GPUCard.tsx
+++ src/dashboard/src/pages/Home/GPUCard.tsx
@@ -131,6 +131,9 @@ const Chart: React.FC<{
         <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none"/>
         <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none"/>
         <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`${value}`}</text>
+        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
+          {`(${(percent * 100).toFixed(2)}%)`}
+        </text>
       </g>
     );
   };
@@ -148,7 +151,7 @@ const Chart: React.FC<{
             activeIndex={activeIndex}
             activeShape={renderActiveShape}
             data={data}
-            cx={230}
+            cx={210}
             // cy={200}
             innerRadius={60}
             outerRadius={80}
