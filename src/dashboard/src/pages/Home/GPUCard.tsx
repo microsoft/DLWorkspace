f65a50f240afffa8fe7448ccec8de4390dec88a1
hongyiliu <hongyiliu1992@gmail.com>
2019-10-21 10:45:35 -0700

    Remove the rate of each pie chart

--- src/dashboard/src/pages/Home/GPUCard.tsx
+++ src/dashboard/src/pages/Home/GPUCard.tsx
@@ -131,9 +131,6 @@ const Chart: React.FC<{
         <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none"/>
         <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none"/>
         <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`${value}`}</text>
-        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
-          {`(Rate ${(percent * 100).toFixed(2)}%)`}
-        </text>
       </g>
     );
   };
