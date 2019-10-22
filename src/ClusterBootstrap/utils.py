70e08f0197fa1b1cddf1ffe2e306f56b6033c7bb
REDMOND\anbhu <anbhu@microsoft.com>
2019-10-22 11:46:15 -0700

    Convert pname to abspath

--- src/ClusterBootstrap/utils.py
+++ src/ClusterBootstrap/utils.py
@@ -400,7 +400,9 @@ def setup_backup_dir(pname):
     deploy_backup_dir = "./deploy_backup"
     backup_dir = os.path.join(deploy_backup_dir, "backup")
 
-    pname_par = os.path.abspath(os.path.join(os.path.abspath(pname), os.pardir))
+    pname = os.path.abspath(pname)
+
+    pname_par = os.path.abspath(os.path.join(pname, os.pardir))
     backup_dir_par = os.path.abspath(os.path.join(os.path.abspath(backup_dir), os.pardir))
 
     assert pname_par != backup_dir_par
