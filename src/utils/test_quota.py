84fc0f84852550f1b47a5071684b7fbfea4abb46
Di Xu <xudifsd@gmail.com>
2019-10-24 05:16:27 +0000

    add test case

--- src/utils/test_quota.py
+++ src/utils/test_quota.py
@@ -15,7 +15,7 @@ class TestQuota(unittest.TestCase):
     """
     Test quota.py
     """
-    def test_gpu_accounting(self):
+    def test_gpu_accounting_idle_gpus_become_unscheduable(self):
         vc_info = {
                 "A": {"P40": 40},
                 "B": {"P40": 40},
@@ -60,6 +60,97 @@ class TestQuota(unittest.TestCase):
 
         self.assertEqual(target_vc_unschedulable, vc_unschedulable)
 
+    def test_gpu_accounting_move_quota_from_one_vc_to_another(self):
+        vc_info = {
+                "A": {"P40": 20},
+                "B": {"P40": 20},
+                }
+
+        # previous A has quota of 30, and A used them all, later admin moved
+        # 10 to B
+        vc_usage = {
+                "A": {"P40": 30},
+                "B": {"P40": 5},
+                }
+
+        cluster_total = {"P40": 40}
+        cluster_available = {"P40": 5}
+        cluster_unschedulable = {}
+
+        result = quota.calculate_vc_gpu_counts(
+                cluster_total,
+                cluster_available,
+                cluster_unschedulable,
+                vc_info,
+                vc_usage)
+
+        vc_total, vc_used, vc_available, vc_unschedulable = result
+
+        self.assertEqual(vc_info, vc_total)
+        self.assertEqual(vc_usage, vc_used)
+
+        target_vc_available = {
+                "A": {"P40": 0},
+                "B": {"P40": 5},
+                }
+
+        self.assertEqual(target_vc_available, vc_available)
+
+        target_vc_unschedulable = {
+                "A": {"P40": -10},
+                "B": {"P40": 10},
+                }
+
+        self.assertEqual(target_vc_unschedulable, vc_unschedulable)
+
+    def test_gpu_accounting_real_case(self):
+        vc_info = {
+                "platform": {"P40": 48},
+                "relevance": {"P40": 200},
+                "quantus": {"P40": 100},
+                "AU": {"P40": 20},
+                }
+
+        vc_usage = {
+                "platform": {"P40": 57},
+                "relevance": {"P40": 164},
+                "quantus": {"P40": 93},
+                "AU": {"P40": 0},
+                }
+
+        cluster_total = {"P40": 368}
+        cluster_available = {"P40": 54}
+        cluster_unschedulable = {}
+
+        result = quota.calculate_vc_gpu_counts(
+                cluster_total,
+                cluster_available,
+                cluster_unschedulable,
+                vc_info,
+                vc_usage)
+
+        vc_total, vc_used, vc_available, vc_unschedulable = result
+
+        self.assertEqual(vc_info, vc_total)
+        self.assertEqual(vc_usage, vc_used)
+
+        target_vc_available = {
+                "platform": {"P40": 0},
+                "relevance": {"P40": 30},
+                "quantus": {"P40": 6},
+                "AU": {"P40": 17},
+                }
+
+        self.assertEqual(target_vc_available, vc_available)
+
+        target_vc_unschedulable = {
+                "platform": {"P40": -9},
+                "relevance": {"P40": 6},
+                "quantus": {"P40": 1},
+                "AU": {"P40": 3},
+                }
+
+        self.assertEqual(target_vc_unschedulable, vc_unschedulable)
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
