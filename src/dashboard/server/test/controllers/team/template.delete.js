0843c2ec0de09c0d18b59927ed8e9a21e066978a
hzzhang <monsttrod@gmail.com>
2019-10-24 16:15:49 +0800

    add test for template deletion

---
+++ src/dashboard/server/test/controllers/team/template.delete.js
@@ -0,0 +1,56 @@
+const axiosist = require('axiosist')
+const sinon = require('sinon')
+const nock = require('nock')
+const config = require('config')
+const api = require('../../../api').callback()
+const User = require('../../../api/services/user')
+
+const clusterConfig = config.get('clusters')
+
+const userParams = {
+  email: 'dlts@example.com',
+  token: User.generateToken('dlts@example.com').toString('hex'),
+  teamId: 'testteam',
+  templateName: 'newtemplate'
+}
+
+const deleteTemplateParams = new URLSearchParams({
+  userName: userParams.email,
+  vcName: userParams.teamId,
+  database: 'user',
+  templateName: userParams.templateName
+})
+
+describe('DELETE /teams/:teamId/templates/:templateName', () => {
+  it('should return 204 if template deleted successfully', async () => {
+    for(let key in clusterConfig) {
+      nock(clusterConfig[key]['restfulapi'])
+        .delete('/templates?' + deleteTemplateParams)
+        .reply(200, {
+          message: 'template deleted successfully'
+        })
+    }
+    sinon.stub(User.prototype, 'fillIdFromWinbind').resolves();
+
+    const response = await axiosist(api).delete('/teams/testteam/templates/newtemplate', {
+      params: userParams
+    })
+
+    response.status.should.equal(204)
+  })
+
+  it('should return 502 if template deleting failed', async () => {
+    for(let key in clusterConfig) {
+      nock(clusterConfig[key]['restfulapi'])
+        .delete('/templates?' + deleteTemplateParams)
+        .reply(500)
+    }
+    sinon.stub(User.prototype, 'fillIdFromWinbind').resolves();
+
+    const response = await axiosist(api).delete('/teams/testteam/templates/newtemplate', {
+      params: userParams
+    })
+
+    response.status.should.equal(502)
+  })
+})
