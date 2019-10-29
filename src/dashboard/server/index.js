c7967e9c889b1f070c8c7b3fb295d01fae3cee76
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2019-10-29 23:02:14 +0800

    Ignore the main module detection in cov test

--- src/dashboard/server/index.js
+++ src/dashboard/server/index.js
@@ -6,6 +6,7 @@ const app = module.exports = new Koa()
 app.use(mount('/api', require('./api')))
 app.use(require('./frontend'))
 
+/* istanbul ignore if */
 if (require.main === module) {
   app.listen(process.env.PORT || 3000, process.env.HOST)
 }
