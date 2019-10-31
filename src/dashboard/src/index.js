0970c4e7314b8913605588864df8fac0de5ca0c1
George Cheng <Qixiang.Cheng@microsoft.com>
2019-10-31 13:16:09 +0800

    Add frontend error logger (#607)

--- src/dashboard/src/index.js
+++ src/dashboard/src/index.js
@@ -6,3 +6,16 @@ import App from './App';
 window.bootstrap = (props) => ReactDOM.render(
   React.createElement(App, props),
   document.getElementById('root'));
+
+window.onerror = (message, source, line, col, error) => {
+  const image = new Image()
+  const queries = []
+  queries.push(`message=${encodeURIComponent(message)}`)
+  queries.push(`source=${encodeURIComponent(source)}`)
+  queries.push(`line=${encodeURIComponent(line)}`)
+  queries.push(`col=${encodeURIComponent(col)}`)
+  if (error.stack) {
+    queries.push(`stack=${encodeURIComponent(error.stack)}`)
+  }
+  image.src = `/api/error.gif?${queries.join('&')}`
+}
