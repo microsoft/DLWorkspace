6903b247307334518025108674ee8023ef9e71b5
hongyiliu <wenhualiu.liu@gmail.com>
2019-11-02 11:36:12 -0700

    Add default team key into request body (#620)

--- src/dashboard/server/test/controllers/cluster/jobs.post.js
+++ src/dashboard/server/test/controllers/cluster/jobs.post.js
@@ -19,12 +19,25 @@ describe('POST /clusters/:clusterid/jobs', () => {
     sinon.stub(User.prototype, 'fillIdFromWinbind').resolves();
 
     const response = await axiosist(api).post('/clusters/Universe/jobs',
-      {team: 'test'}, {params: userParams})
+      { vcName: 'test' }, { params: userParams })
 
     response.status.should.equal(200)
     response.data.should.have.property('message', 'job adding succeeded')
   })
+  it('should response returning messages if job POST succeeded', async () => {
+    nock('http://universe')
+      .post('/PostJob')
+      .reply(200, {
+        message: 'job adding succeeded'
+      })
+    sinon.stub(User.prototype, 'fillIdFromWinbind').resolves();
 
+    const response = await axiosist(api).post('/clusters/Universe/jobs',
+      { vcName: 'test', team: null }, { params: userParams })
+
+    response.status.should.equal(200)
+    response.data.should.have.property('message', 'job adding succeeded')
+  })
   it('should response 400 Bad Request if job schema is invalid', async () => {
     sinon.stub(User.prototype, 'fillIdFromWinbind').resolves();
 
