3682eddcb88d039f796cf9f4cf3cac3e3a44a357
anbhu <5781796+Anbang-Hu@users.noreply.github.com>
2019-11-06 16:04:44 -0800

    Move pure CPU jobs to CPU machines (#640)

--- src/ClusterBootstrap/scripts/docker_network_gc_setup.sh
+++ src/ClusterBootstrap/scripts/docker_network_gc_setup.sh
@@ -3,15 +3,16 @@
 cp /etc/sysctl.conf /tmp/sysctl.conf
 
 # Force gc to clean-up quickly
-grep -xF "net.ipv4.neigh.default.gc_interval = 3600" /tmp/sysctl.conf || echo "net.ipv4.neigh.default.gc_interval = 3600" | sudo tee -a /tmp/sysctl.conf
+grep -xF "net.ipv4.neigh.default.gc_interval = 1800" /tmp/sysctl.conf || echo "net.ipv4.neigh.default.gc_interval = 1800" | sudo tee -a /tmp/sysctl.conf
 
 # Set ARP cache entry timeout
-grep -xF "net.ipv4.neigh.default.gc_stale_time = 3600" /tmp/sysctl.conf || echo "net.ipv4.neigh.default.gc_stale_time = 3600" | sudo tee -a  /tmp/sysctl.conf
+grep -xF "net.ipv4.neigh.default.gc_stale_time = 1800" /tmp/sysctl.conf || echo "net.ipv4.neigh.default.gc_stale_time = 1800" | sudo tee -a  /tmp/sysctl.conf
 
 # Setup DNS threshold for arp
-grep -xF "net.ipv4.neigh.default.gc_thresh3 = 8192" /tmp/sysctl.conf || echo "net.ipv4.neigh.default.gc_thresh3 = 8192" | sudo tee -a /tmp/sysctl.conf
-grep -xF "net.ipv4.neigh.default.gc_thresh2 = 4096" /tmp/sysctl.conf || echo "net.ipv4.neigh.default.gc_thresh2 = 4096" | sudo tee -a /tmp/sysctl.conf
-grep -xF "net.ipv4.neigh.default.gc_thresh1 = 2048" /tmp/sysctl.conf || echo "net.ipv4.neigh.default.gc_thresh1 = 2048" | sudo tee -a /tmp/sysctl.conf
+# Check ARP Cache section of post: https://openai.com/blog/scaling-kubernetes-to-2500-nodes
+grep -xF "net.ipv4.neigh.default.gc_thresh1 = 80000" /tmp/sysctl.conf || echo "net.ipv4.neigh.default.gc_thresh1 = 80000" | sudo tee -a /tmp/sysctl.conf
+grep -xF "net.ipv4.neigh.default.gc_thresh2 = 90000" /tmp/sysctl.conf || echo "net.ipv4.neigh.default.gc_thresh2 = 90000" | sudo tee -a /tmp/sysctl.conf
+grep -xF "net.ipv4.neigh.default.gc_thresh3 = 100000" /tmp/sysctl.conf || echo "net.ipv4.neigh.default.gc_thresh3 = 100000" | sudo tee -a /tmp/sysctl.conf
 
 sudo cp /tmp/sysctl.conf /etc/sysctl.conf
 
--- src/init-scripts/init_user.sh
+++ src/init-scripts/init_user.sh
@@ -14,7 +14,7 @@ apt-get update && apt-get install sudo openssl -y
 # setup user and group, fix permissions
 addgroup --force-badname --gid  ${DLWS_GID} domainusers
 adduser --force-badname --home /home/${DLWS_USER_NAME} --shell /bin/bash --uid ${DLWS_UID}  -gecos '' --gid ${DLWS_GID} --disabled-password ${DLWS_USER_NAME}
-#usermod -p $(echo tryme2017 | openssl passwd -1 -stdin) ${DLWS_USER_NAME}
+usermod -p $(echo tryme2017 | openssl passwd -1 -stdin) ${DLWS_USER_NAME}
 chown ${DLWS_USER_NAME} /home/${DLWS_USER_NAME}/ /home/${DLWS_USER_NAME}/.profile /home/${DLWS_USER_NAME}/.ssh || /bin/true
 chmod 700 /home/${DLWS_USER_NAME}/.ssh || /bin/true
 
