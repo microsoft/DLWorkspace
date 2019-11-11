eaaebe78cd06160a6cb63b889bfe8bcda188a29a
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2019-11-11 11:23:50 +0800

    Dashboard: support HTTPS

--- src/dashboard/server/index.js
+++ src/dashboard/server/index.js
@@ -7,5 +7,24 @@ app.use(mount('/api', require('./api')))
 app.use(require('./frontend'))
 
 if (require.main === module) {
-  app.listen(process.env.PORT || 3000, process.env.HOST)
+  const http = require('http')
+  const https = require('https')
+  const fs = require('fs')
+
+  const {
+    HOST,
+    PORT = 3000,
+    TLS_KEY,
+    TLS_CERT
+  } = process.env
+
+  const server = TLS_KEY && TLS_CERT
+    ? https.createServer({
+      key: fs.readFileSync(TLS_KEY),
+      cert: fs.readFileSync(TLS_CERT)
+    })
+    : http.createServer()
+
+  server.on('request', app.callback())
+  server.listen(PORT, HOST)
 }
