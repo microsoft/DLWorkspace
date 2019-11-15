2e61a7c70a9d336a87e4e1d62b0887f3fe75daee
REDMOND\anbhu <anbhu@microsoft.com>
2019-11-14 21:46:06 -0800

    Add storage monitor

---
+++ src/storage_monitor/scan.py
@@ -0,0 +1,153 @@
+import os
+import sys
+import argparse
+import logging
+import time
+
+from datetime import datetime
+
+
+class PathNode(object):
+    def __init__(self,
+                 path,
+                 isdir,
+                 size,
+                 atime,
+                 children=None):
+        self.path = path
+        self.isdir = isdir
+        self.size = size
+        self.subtree_size = size
+        self.atime = atime
+        self.children = []
+        if isinstance(children, list):
+            self.children = children
+        self.num_subtree_nodes = 1
+
+    def __str__(self):
+        path_type = "Directory" if self.isdir else "File"
+        return "%s %s: size %d, subtree_size %d, atime %s" % \
+               (path_type,
+                self.path,
+                self.size,
+                self.subtree_size,
+                self.atime.strftime("%Y/%m/%d %H:%M:%S"))
+
+
+def create_node(node_path):
+    node_stat = os.stat(node_path)
+    node_isdir = os.path.isdir(node_path)
+    node_size = node_stat.st_size
+    node_atime = datetime.fromtimestamp(node_stat.st_atime)
+    node = PathNode(path=node_path,
+                    isdir=node_isdir,
+                    size=node_size,
+                    atime=node_atime)
+    return node
+
+
+def create_tree(root):
+    if os.path.islink(root):
+        return None
+
+    try:
+        pathnames = os.listdir(root)
+    except Exception as e:
+        logging.warn("Ignore path %s due to exception %s" % (root, e))
+        return None
+
+    root_node = create_node(root)
+
+    dirs, nondirs = [], []
+    for pathname in pathnames:
+        path = os.path.join(root, pathname)
+        if os.path.islink(path):
+            continue
+
+        if os.path.isdir(path):
+            dirs.append(pathname)
+        else:
+            nondirs.append(pathname)
+
+    for pathname in dirs:
+        child_dir = os.path.join(root, pathname)
+        child_dir_node = create_tree(child_dir)
+        if child_dir_node is not None:
+            root_node.children.append(child_dir_node)
+            root_node.subtree_size += child_dir_node.subtree_size
+            root_node.num_subtree_nodes += child_dir_node.num_subtree_nodes
+
+    for pathname in nondirs:
+        child_file = os.path.join(root, pathname)
+        path_node = create_node(child_file)
+        root_node.children.append(path_node)
+        root_node.subtree_size += path_node.subtree_size
+        root_node.num_subtree_nodes += path_node.num_subtree_nodes
+
+    return root_node
+
+
+def print_tree_preorder(root):
+    if root is None:
+        return
+
+    logging.info(root)
+
+    for child in root.children:
+        print_tree_preorder(child)
+
+
+def find_overweight_nodes(root, threshold):
+    if root is None:
+        return []
+
+    if not root.isdir:
+        if root.subtree_size > threshold:
+            return [root]
+
+    if root.subtree_size <= threshold:
+        return []
+
+    overweight_children = []
+
+    if root.subtree_size > threshold:
+        overweight_children.append(root)
+
+    for child in root.children:
+        overweight_children += find_overweight_nodes(child, threshold)
+
+    return overweight_children
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", default=".", type=str)
+    parser.add_argument("--threshold", default=4096, type=int,
+                        help="Path with size greater than the threshold is considered overweight")
+
+    args, _ = parser.parse_known_args()
+    path = args.path
+    threshold = args.threshold
+
+    logging.info("Creating tree rooted at %s starts." % path)
+    root = create_tree(path)
+    logging.info("Creating tree rooted at %s finishes." % path)
+
+    logging.info("Total number of paths found: %d" % root.num_subtree_nodes)
+
+    #print_tree_preorder(root)
+    overweight_nodes = find_overweight_nodes(root, threshold)
+    logging.info("Overweight (> %d) paths are:" % threshold)
+    for node in overweight_nodes:
+        logging.info("%16d  %s" % (node.subtree_size, node.path))
+
+
+if __name__ == "__main__":
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter("[%(levelname)s %(asctime)s] %(message)s")
+    handler.setFormatter(formatter)
+    root_logger.addHandler(handler)
+    main()
---
+++ src/storage_monitor/test_scan.py
@@ -0,0 +1,140 @@
+"""
+Tests for methods in scan.py
+"""
+import shutil
+import platform
+
+from scan import *
+from unittest import TestCase
+
+
+SYSTEM = platform.system()
+WINDOWS = "Windows"
+LINUX = "Linux"
+
+EMPTY_DIR_SIZE = 4096 if SYSTEM is LINUX else 0
+
+
+class TestScan(TestCase):
+
+    def setUp(self):
+        self.test_dir = "test_dir"
+        self.dir1 = os.path.join(self.test_dir, "dir1")
+        self.dir2 = os.path.join(self.test_dir, "dir2")
+
+        if os.path.exists(self.test_dir):
+            self.tearDown()
+
+        os.mkdir(self.test_dir)
+        os.mkdir(self.dir1)
+        os.mkdir(self.dir2)
+
+        self.file2_1 = os.path.join(self.dir2, "file2_1")
+        self.file2_2 = os.path.join(self.dir2, "file2_2")
+        self.file1 = os.path.join(self.test_dir, "file1")
+
+        self.file2_1_len = 8100
+        self.file2_2_len = 500
+        self.file1_len = 10240
+
+        with open(self.file2_1, "wb") as f:
+            f.write("a" * self.file2_1_len)
+
+        with open(self.file2_2, "wb") as f:
+            f.write("a" * self.file2_2_len)
+
+        with open(self.file1, "wb") as f:
+            f.write("a" * self.file1_len)
+
+    def tearDown(self):
+        try:
+            shutil.rmtree(self.test_dir)
+        except:
+            print("Error while deleting directory %s" % self.test_dir)
+
+    def test_create_node(self):
+        file_node = create_node(self.file1)
+        self.assertEqual(self.file1, file_node.path)
+        self.assertFalse(file_node.isdir)
+        self.assertEqual(self.file1_len, file_node.size)
+        self.assertEqual(self.file1_len, file_node.subtree_size)
+        self.assertEqual([], file_node.children)
+        self.assertEqual(1, file_node.num_subtree_nodes)
+
+        dir_node = create_node(self.dir1)
+        self.assertEqual(self.dir1, dir_node.path)
+        self.assertTrue(dir_node.isdir)
+        self.assertEqual(EMPTY_DIR_SIZE, dir_node.size)
+        self.assertEqual(EMPTY_DIR_SIZE, dir_node.subtree_size)
+        self.assertEqual([], dir_node.children)
+        self.assertEqual(1, dir_node.num_subtree_nodes)
+
+    def test_invalid_create_tree(self):
+        self.assertIsNone(create_tree("does_not_exist"))
+        self.assertIsNone(create_tree(self.file1))
+
+    def test_create_tree(self):
+        test_dir = create_tree(self.test_dir)
+
+        # Check test_dir
+        self.assertEqual(self.test_dir, test_dir.path)
+        self.assertTrue(test_dir.isdir)
+        self.assertEqual(EMPTY_DIR_SIZE, test_dir.size)
+        self.assertEqual(3 * EMPTY_DIR_SIZE + self.file2_1_len +
+                         self.file2_2_len + self.file1_len,
+                         test_dir.subtree_size)
+        self.assertEqual(3, len(test_dir.children))
+        self.assertEqual(6, test_dir.num_subtree_nodes)
+
+        # Check dir1
+        dir1 = test_dir.children[0]
+        self.assertEqual(self.dir1, dir1.path)
+        self.assertTrue(dir1.isdir)
+        self.assertEqual(EMPTY_DIR_SIZE, dir1.size)
+        self.assertEqual(EMPTY_DIR_SIZE, dir1.subtree_size)
+        self.assertEqual(0, len(dir1.children))
+        self.assertEqual(1, dir1.num_subtree_nodes)
+
+        # Check dir2
+        dir2 = test_dir.children[1]
+        self.assertEqual(self.dir2, dir2.path)
+        self.assertTrue(dir2.isdir)
+        self.assertEqual(EMPTY_DIR_SIZE, dir2.size)
+        self.assertEqual(EMPTY_DIR_SIZE + self.file2_1_len +
+                         self.file2_2_len,
+                         dir2.subtree_size)
+        self.assertEqual(2, len(dir2.children))
+        self.assertEqual(3, dir2.num_subtree_nodes)
+
+        # Check file1
+        file1 = test_dir.children[2]
+        self.assertEqual(self.file1, file1.path)
+        self.assertFalse(file1.isdir)
+        self.assertEqual(self.file1_len, file1.size)
+        self.assertEqual(self.file1_len, file1.subtree_size)
+        self.assertEqual(0, len(file1.children))
+        self.assertEqual(1, file1.num_subtree_nodes)
+
+        # Check file2_1
+        file2_1 = dir2.children[0]
+        self.assertEqual(self.file2_1, file2_1.path)
+        self.assertFalse(file2_1.isdir)
+        self.assertEqual(self.file2_1_len, file2_1.size)
+        self.assertEqual(self.file2_1_len, file2_1.subtree_size)
+        self.assertEqual(0, len(file2_1.children))
+        self.assertEqual(1, file2_1.num_subtree_nodes)
+
+        # Check file2_2
+        file2_2 = dir2.children[1]
+        self.assertEqual(self.file2_2, file2_2.path)
+        self.assertFalse(file2_2.isdir)
+        self.assertEqual(self.file2_2_len, file2_2.size)
+        self.assertEqual(self.file2_2_len, file2_2.subtree_size)
+        self.assertEqual(0, len(file2_2.children))
+        self.assertEqual(1, file2_2.num_subtree_nodes)
+
+    def test_find_overweight_nodes(self):
+        test_dir = create_tree(self.test_dir)
+        overweight_nodes = find_overweight_nodes(test_dir, 10000)
+        self.assertIn(test_dir, overweight_nodes)
+        self.assertIn(test_dir.children[2], overweight_nodes)
