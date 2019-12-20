cb97d0f22522bb2851b60f3c1f640cf107036eb0
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2019-12-20 01:20:16 +0000

    v2 API

---
+++ src/dashboard/server/api/v2/services/cluster.js
@@ -0,0 +1,58 @@
+const ClusterV1 = require('../../services/cluster')
+
+/**
+ * @typedef {Object} State
+ * @property {import('./user')} user
+ */
+
+/**
+ * @extends {ClusterV1<State>}
+ */
+class Cluster extends ClusterV1 {
+
+  /**
+   * @param {string} teamId
+   * @param {boolean} all
+   * @param {number} limit
+   * @return {Promise<Array>}
+   */
+  async getJobs (teamId, all, limit) {
+    const { user } = this.context.state
+    const params = new URLSearchParams({
+      userName: user.email,
+      vcName: teamId,
+      jobOwner: all ? 'all' : user.email,
+      num: limit
+    })
+    const response = await this.fetch('/ListJobsV2?' + params)
+    this.context.assert(response.ok, 502)
+    const data = await response.json()
+    const jobs = [].concat(
+      data['finishedJobs'],
+      data['queuedJobs'],
+      data['runningJobs'],
+      data['visualizationJobs']
+    )
+    this.context.log.info('Got %d jobs from %s', jobs.length, this.id)
+    return jobs
+  }
+
+  /**
+   * @param {string} jobId
+   * @return {Promise}
+   */
+  async getJob (jobId) {
+    const { user } = this.context.state
+    const params = new URLSearchParams({
+      userName: user.email,
+      jobId
+    })
+    const response = await this.fetch('/GetJobDetail?' + params)
+    this.context.assert(response.ok, 502)
+    const job = await response.json()
+    this.context.log.debug('Got job %s', job['jobName'])
+    return job
+  }
+}
+
+module.exports = Cluster
