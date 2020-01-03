b73cda28400f6dfb51bc6bc92e1bb0e00240620e
REDMOND\anbhu <anbhu@microsoft.com>
2020-01-02 21:29:49 -0800

    run storagemanager process

--- src/docker-images/StorageManager/run.sh
+++ src/docker-images/StorageManager/run.sh
@@ -2,5 +2,4 @@
 rm -f /DLWorkspace/src/StorageManager/config.yaml
 ln -s /StorageManager/config.yaml /DLWorkspace/src/StorageManager/config.yaml
 
-sleep infinity
-#python3 /DLWorkspace/src/StorageManager/main.py
+python3 /DLWorkspace/src/StorageManager/main.py
