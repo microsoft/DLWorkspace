6e28335dc30c760851efaa56641ef4edf88c007f
Di Xu <xudifsd@gmail.com>
2020-01-04 04:26:02 +0000

    Revert "fix previous bug (#737)"
    This reverts commit cdb176ce3cd152199aed9737bfc988bf4dee9792.

--- src/ClusterBootstrap/deploy.py
+++ src/ClusterBootstrap/deploy.py
@@ -3137,7 +3137,7 @@ def start_kube_service( servicename ):
                     time.sleep(int(filename.split(" ")[1]))
                 else:
                     filename = filename.strip('\n')
-                    start_one_kube_service(os.path.join(dirname,filename), config_dir)
+                    start_one_kube_service(os.path.join(dirname,filename))
     else:
         start_one_kube_service(fname)
 
