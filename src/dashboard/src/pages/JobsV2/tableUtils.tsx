163f61851d874751f3080505daa9f51ef9a817f3
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2020-01-09 06:33:44 +0000

    Fix GPU rendering

--- src/dashboard/src/pages/JobsV2/tableUtils.tsx
+++ src/dashboard/src/pages/JobsV2/tableUtils.tsx
@@ -10,6 +10,20 @@ export const renderId = (job: any) => (
   </UILink>
 )
 
+const getGPU = (job: any) => {
+  const type = job['jobParams']['jobtrainingtype'];
+  if (type == null || type === 'RegularJob' || type === 'InferenceJob') {
+    return job['jobParams']['resourcegpu']
+  } else {
+    return job['jobParams']['resourcegpu'] * job['jobParams']['numpsworker']
+  }
+}
+
+export const renderGPU = (job: any) => <>{getGPU(job)}</>;
+export const sortGPU = (jobA: any, jobB: any) => {
+  return getGPU(jobA) - getGPU(jobB)
+};
+
 export const renderStatus = (job: any) => <JobStatus job={job}/>;
 
 export const renderDate = (getter: (job: any) => Date) => (job: any) => {
