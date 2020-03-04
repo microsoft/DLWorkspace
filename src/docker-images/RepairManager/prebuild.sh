78a46576beed30109cb795787327ebb3cb049cf8
Zhe Xu <zhexu@microsoft.com>
2020-03-04 21:26:18 +0000

    update cloudinit to support repairmanager

--- src/docker-images/RepairManager/prebuild.sh
+++ src/docker-images/RepairManager/prebuild.sh
@@ -3,5 +3,4 @@
 # src/ClusterBootstrap/deploy/docker-images/.../
 rm -rf RepairManager
 cp -r ../../../../RepairManager RepairManager
-
 cp -r ../../../../ClusterBootstrap/deploy/bin/kubectl kubectl
