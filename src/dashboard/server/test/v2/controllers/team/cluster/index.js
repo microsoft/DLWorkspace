861bd099980c8a06e78f167c9e826171447bbf0b
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2020-03-09 06:13:20 +0000

    Unit test

---
+++ src/dashboard/server/test/v2/controllers/team/cluster/index.js
@@ -0,0 +1,33 @@
+const axiosist = require('axiosist')
+const nock = require('nock')
+const api = require('../../../../../api').callback()
+const User = require('../../../../../api/services/user')
+
+const EMAIL = 'dlts@example.com'
+const PASSWORD = User.generateToken(EMAIL).toString('hex')
+const TEAM_ID = 'theTeam'
+
+describe('GET /teams/:teamId/clusters/:clusterId', () => {
+  it.only('should return cluster status', async () => {
+    nock('http://Universe')
+      .get('/GetVC?' + new URLSearchParams({ userName: EMAIL, vcName: TEAM_ID }))
+      .reply(200, {
+        AvaliableJobNum: 0
+      })
+
+    const response = await axiosist(api).get(`/v2/teams/${TEAM_ID}/clusters/Universe`, {
+      params: {
+        email: EMAIL,
+        password: PASSWORD
+      }
+    })
+
+    response.status.should.equal(200)
+    response.data.should.have.property('config')
+    response.data.config.should.have.property('grafana', 'http://grafana.universe')
+
+    response.data.should.have.property('runningJobs', 0)
+
+    // TODO: Add more field validation
+  })
+})
