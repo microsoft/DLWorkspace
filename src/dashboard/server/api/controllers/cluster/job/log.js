cd243b0f87d9e863b41335e8c075274269945ce7
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2020-03-27 18:43:33 +0000

    Dashboard backend: add a 60 seconds cache

--- src/dashboard/server/api/controllers/cluster/job/log.js
+++ src/dashboard/server/api/controllers/cluster/job/log.js
@@ -3,11 +3,22 @@
  * @property {import('../../../services/cluster')} cluster
  */
 
+const CACHE_CONTROL = 'private, max-age=60'
+
 /** @type {import('koa').Middleware<State>} */
 module.exports = async context => {
   const { cluster } = context.state
   const { jobId } = context.params
   const { cursor } = context.query
 
-  context.body = await cluster.getJobLog(jobId, cursor)
+  context.set('cache-control', CACHE_CONTROL)
+
+  try {
+    context.body = await cluster.getJobLog(jobId, cursor)
+  } catch (error) {
+    if (error.status === 404) {
+      error.headers = {'cache-control': CACHE_CONTROL}
+    }
+    throw error
+  }
 }
