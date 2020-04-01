4a0e400093b38dc7b825161cbc44b88e264a57fc
George Cheng <Qixiang.Cheng@microsoft.com>
2020-04-02 01:01:53 +0800

    Logging: issue fixes of azure blob backend. (#992)
    * Fix logging
    
    * Always retry sending to adapter
    
    * Sending data instead of piping stream
    
    ...to avoid empty second piping.
    
    * Fix merge conflict with bring log field back
    
    * Get log by chunk (1MB)
    
    * __extract_job_log_legacy have higher priority

--- src/docker-images/azure-blob-adapter/wsgi.py
+++ src/docker-images/azure-blob-adapter/wsgi.py
@@ -1,14 +1,18 @@
 from azure.storage.blob import AppendBlobService
 from azure.common import AzureMissingResourceHttpError, AzureHttpError
 from dotenv import load_dotenv
-from logging import getLogger
+from logging import getLogger, StreamHandler
 from os import environ
+from sys import stdout
 from werkzeug.wrappers import PlainRequest, Response
 
 __all__ = ['application']
 
 load_dotenv()
+
 logger = getLogger(__name__)
+logger.setLevel('INFO')
+logger.addHandler(StreamHandler(stdout))
 
 append_blob_service = AppendBlobService(connection_string=environ['AZURE_STORAGE_CONNECTION_STRING'])
 container_name = environ['AZURE_STORAGE_CONTAINER_NAME']
@@ -22,24 +26,24 @@ def application(request):
     try:
         blob_name = request.headers['X-Tag']
     except KeyError:
-        logger.exception()
+        logger.exception('Key Error')
         return Response(status=400)
 
     def append_blob():
-        resource_properties = append_blob_service.append_blob_from_stream(
+        resource_properties = append_blob_service.append_blob_from_bytes(
             container_name=container_name,
             blob_name=blob_name,
-            stream=request.stream,
+            blob=request.get_data(),
             count=request.content_length)
-        logger.info('Successfully append to blob {} in container {}'.format(
-            blob_name, container_name), extra=resource_properties)
+        logger.info('Successfully append {} bytes to blob {} in container {}: {}'.format(
+            request.content_length, blob_name, container_name, resource_properties.etag))
 
     def create_blob():
         resource_properties = append_blob_service.create_blob(
             container_name=container_name,
             blob_name=blob_name)
-        logger.info('Successfully create blob {} in container {}'.format(
-            blob_name, container_name), extra=resource_properties)
+        logger.info('Successfully create blob {} in container {}: {}'.format(
+            blob_name, container_name, resource_properties.etag))
 
     try:
         try:
@@ -49,8 +53,8 @@ def application(request):
             append_blob()
         return Response(status=201)
     except AzureHttpError:
-        logger.exception()
+        logger.exception('Azure HTTP Error')
         return Response(status=502)
     except Exception:
-        logger.exception()
+        logger.exception('Exception')
         return Response(status=500)
