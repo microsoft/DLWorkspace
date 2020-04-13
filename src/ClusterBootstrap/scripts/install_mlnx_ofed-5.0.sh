a46d4dd329799f471452ec97f19cfe286dea0e21
Anbang Hu <anbhu@microsoft.com>
2020-04-13 14:48:52 -0700

    Remove MLNX directory

--- src/ClusterBootstrap/scripts/install_mlnx_ofed-5.0.sh
+++ src/ClusterBootstrap/scripts/install_mlnx_ofed-5.0.sh
@@ -9,6 +9,7 @@ export STAGE_DIR=/tmp
 
 sudo mkdir -p ${STAGE_DIR}
 cd ${STAGE_DIR}
+sudo rm -rf MLNX_OFED_LINUX-5.0-1.0.0.0-ubuntu18.04-x86_64
 sudo wget -q -O - http://www.mellanox.com/downloads/ofed/MLNX_OFED-5.0-1.0.0.0/MLNX_OFED_LINUX-5.0-1.0.0.0-ubuntu18.04-x86_64.tgz | sudo tar xzf -
 cd MLNX_OFED_LINUX-5.0-1.0.0.0-ubuntu18.04-x86_64
 sudo ./mlnxofedinstall --force
