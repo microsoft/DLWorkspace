ed864d8b08c580436b619373919f704699b7f0f1
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2020-05-12 14:25:11 +0000

    Disable bfcache in bootstrap.js

--- src/dashboard/server/api/controllers/bootstrap.js
+++ src/dashboard/server/api/controllers/bootstrap.js
@@ -13,6 +13,7 @@ const frontendConfig = config.has('frontend')
 module.exports = (context) => {
   const { user } = context.state
   const parameter = { config: frontendConfig, user }
+  context.set('Cache-Control', 'no-cache, no-store, must-revalidate')
   context.type = 'js'
   context.body = `bootstrap(${JSON.stringify(parameter)})`
 }
