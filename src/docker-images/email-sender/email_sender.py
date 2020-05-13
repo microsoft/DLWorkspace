3c8678e9d3e3b86f7c60ab03a9647eb4900b55f9
Di Xu <xudifsd@gmail.com>
2020-05-12 18:48:10 -0700

    fix severl error (#1100)

--- src/docker-images/email-sender/email_sender.py
+++ src/docker-images/email-sender/email_sender.py
@@ -29,50 +29,56 @@ def load_smtp_config(path):
             "default_cc")
 
 
-def run(args):
+def gen_callback(smtp_config_path):
     smtp_url, smtp_from, smtp_user, smtp_pass, default_cc = load_smtp_config(
-        args.smtp)
+        smtp_config_path)
+
+    def callback(ch, method, properties, body):
+        with smtplib.SMTP(smtp_url) as smtp_conn:
+            smtp_conn.starttls()
+            smtp_conn.login(smtp_user, smtp_pass)
+            logger.info("received %r" % body)
+            try:
+                jbody = json.loads(body)
+                params = payload.Payload.deserialize(jbody)
+                msg = params.to_email_message(smtp_from, default_cc)
+            except Exception:
+                logger.exception("error when parsing body %s, drop it", body)
+                # drop malformed message
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+                return
+
+            try:
+                smtp_conn.send_message(msg)
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+            except smtplib.SMTPServerDisconnected:
+                logger.error("failed to connect to smtp server, retain message")
+            except Exception:
+                logger.exception("error when sending email %s", body)
+
+    return callback
+
+
+def run(args):
+    callback = gen_callback(args.smtp)
 
     credentials = pika.PlainCredentials(args.mq_user, args.mq_pass)
 
     while True:
         try:
-            with smtplib.SMTP(smtp_url) as smtp_conn:
-                smtp_conn.starttls()
-                smtp_conn.login(smtp_user, smtp_pass)
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=args.host,
+                                          credentials=credentials))
+            channel = connection.channel()
 
-                connection = pika.BlockingConnection(
-                    pika.ConnectionParameters(host=args.host,
-                                              credentials=credentials))
-                channel = connection.channel()
+            channel.queue_declare(queue=args.queue, durable=True)
+            logger.info("waiting for messages")
 
-                channel.queue_declare(queue=args.queue, durable=True)
-                logger.info("waiting for messages")
+            channel.basic_qos(prefetch_count=500)
+            channel.basic_consume(queue=args.queue,
+                                  on_message_callback=callback)
 
-                def callback(ch, method, properties, body):
-                    logger.info("received %r" % body)
-                    try:
-                        jbody = json.loads(body)
-                        params = payload.Payload.deserialize(jbody)
-                        msg = params.to_email_message(smtp_from, default_cc)
-                    except Exception:
-                        logger.exception("error when parsing body %s, drop it",
-                                         body)
-                        # drop malformed message
-                        ch.basic_ack(delivery_tag=method.delivery_tag)
-                        return
-
-                    try:
-                        smtp_conn.send_message(msg)
-                        ch.basic_ack(delivery_tag=method.delivery_tag)
-                    except Exception:
-                        logger.exception("error when sending email %s", body)
-
-                channel.basic_qos(prefetch_count=500)
-                channel.basic_consume(queue=args.queue,
-                                      on_message_callback=callback)
-
-                channel.start_consuming()
+            channel.start_consuming()
         except pika.exceptions.ProbableAuthenticationError:
             logger.error("authorization failed")
             return 1
