d3a4cbf328516cb37acfef10e56ed320d76030bb
Qixiang Cheng <Qixiang.Cheng@microsoft.com>
2020-05-16 02:23:23 +0000

    Refine column titla

---
+++ src/dashboard/src/components/CaptionColumnTitle.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {
+  FunctionComponent
+} from 'react';
+
+import {
+  Box,
+  Typography
+} from '@material-ui/core';
+
+interface Props {
+  caption?: string;
+}
+
+const ColumnTitle: FunctionComponent<Props> = ({ caption, children }) => (
+  <Box>
+    {children && <Typography variant="inherit" component="p">{children}</Typography>}
+    {caption && <Typography variant="caption" component="p">{caption}</Typography>}
+  </Box>
+)
+
+export default ColumnTitle;
