555ab7e958da1bc1d895479b3f8d46078b21893a
Di Xu <xudifsd@gmail.com>
2020-05-28 16:10:37 -0700

    broadcast signal in bash for faster termination (#1139)

--- src/docker-images/RestfulAPI/runScheduler.sh
+++ src/docker-images/RestfulAPI/runScheduler.sh
@@ -2,6 +2,14 @@
 
 set -x
 
+_broadcast() {
+  kill -SIGTERM -"$cur_pgid"
+}
+
+cur_pgid=`ps -o '%r' $$ | tail -n 1 | sed "s/ //g"`
+
+trap _broadcast SIGTERM
+
 kubectl create priorityclass job-priority --value=1000 --description="non-preemptible job pod priority" --dry-run -o yaml | kubectl replace --force=true -f -
 kubectl create priorityclass preemptible-job-priority --value=500 --description="preemptible job pod priority" --dry-run -o yaml | kubectl replace --force=true -f -
 kubectl create priorityclass inference-job-priority --value=100 --description="inference job priority" --dry-run -o yaml | kubectl replace --force=true -f -
@@ -13,4 +21,6 @@ export KUBE_SERVER_VERSION=`kubectl version | gawk 'match($0, /Server Version:.*
 ln -s /RestfulAPI/config.yaml /DLWorkspace/src/utils/config.yaml
 
 cd /DLWorkspace/src/ClusterManager
-python3 /DLWorkspace/src/ClusterManager/cluster_manager.py
+
+python3 /DLWorkspace/src/ClusterManager/cluster_manager.py &
+wait # for SIGTERM to deliver
