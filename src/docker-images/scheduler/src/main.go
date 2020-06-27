3668d362b1cfd93f573ac73a7f9d2166dadea15a
Di Xu <xudifsd@gmail.com>
2020-06-27 06:09:47 +0000

    change from pod watch list to pod informer

--- src/docker-images/scheduler/src/main.go
+++ src/docker-images/scheduler/src/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
+	listerV1 "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/klog"
@@ -13,7 +13,9 @@ import (
 const SCHEDULER_NAME = "DLTS"
 
 type Scheduler struct {
-	k8sClient *kubernetes.Clientset
+	k8sClient  *kubernetes.Clientset
+	podLister  listerV1.PodLister
+	nodeLister listerV1.NodeLister
 }
 
 func NewScheduler() *Scheduler {
@@ -34,62 +36,64 @@ func (s *Scheduler) Run(stopCh chan struct{}) {
 	stopListPods := make(chan struct{})
 	stopListNodes := make(chan struct{})
 
-	s.listPods(stopListPods)
-	s.listNodes(stopListNodes)
+	s.podLister = s.listPods(stopListPods)
+	s.nodeLister = s.listNodes(stopListNodes)
+
 	<-stopCh
+
 	stopListPods <- struct{}{}
 	stopListNodes <- struct{}{}
 }
 
-func (s *Scheduler) listPods(stopCh chan struct{}) {
-	// list all pods from all namespaces and schedulerName is DLTS and nodeName is unassigned
-	watch, err := s.k8sClient.CoreV1().Pods("").Watch(metav1.ListOptions{
-		// FieldSelector: fmt.Sprintf("spec.schedulerName=%s,spec.nodeName=", SCHEDULER_NAME),
+func (s *Scheduler) listPods(stopCh chan struct{}) listerV1.PodLister {
+	factory := informers.NewSharedInformerFactory(s.k8sClient, 0)
+	pods := factory.Core().V1().Pods()
+	informer := pods.Informer()
+
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    s.podAdded,
+		UpdateFunc: s.podUpdated,
+		DeleteFunc: s.podDeleted,
 	})
-	if err != nil {
-		panic(err.Error())
-	}
 
-	go func(stopCh chan struct{}) {
-		resultChan := watch.ResultChan()
-
-		for {
-			select {
-			case <-stopCh:
-				break
-			case event := <-resultChan:
-				p := event.Object.(*v1.Pod)
-
-				switch event.Type {
-				case "ADDED":
-					s.podAdded(p)
-				case "MODIFIED":
-					s.podModified(p)
-				case "DELETED":
-					s.podDeleted(p)
-				default:
-					klog.Warningf("unknown event %s for pod %s/%s", event.Type, p.Namespace, p.Name)
-				}
-			}
-		}
-	}(stopCh)
+	go informer.Run(stopCh)
+	return pods.Lister()
 }
 
-func (s *Scheduler) podAdded(pod *v1.Pod) {
+func (s *Scheduler) podAdded(obj interface{}) {
+	pod, ok := obj.(*v1.Pod)
+	if !ok {
+		klog.Warningf("failed to cast obj to pod %v", obj)
+		return
+	}
 	klog.Infof("pod added: %s/%s", pod.Namespace, pod.Name)
 }
 
-func (s *Scheduler) podModified(pod *v1.Pod) {
-	klog.Infof("pod modified: %s/%s", pod.Namespace, pod.Name)
+func (s *Scheduler) podUpdated(oldObj, newObj interface{}) {
+	oldPod, ok1 := oldObj.(*v1.Pod)
+	newPod, ok2 := newObj.(*v1.Pod)
+
+	if !ok1 || !ok2 {
+		klog.Warningf("failed to cast obj to node %v %v", oldObj, newObj)
+		return
+	}
+	klog.Infof("pod updated: %s/%s -> %s/%s", oldPod.Namespace, oldPod.Name,
+		newPod.Namespace, newPod.Name)
 }
 
-func (s *Scheduler) podDeleted(pod *v1.Pod) {
+func (s *Scheduler) podDeleted(obj interface{}) {
+	pod, ok := obj.(*v1.Pod)
+	if !ok {
+		klog.Warningf("failed to cast obj to pod %v", obj)
+		return
+	}
 	klog.Infof("pod deleted: %s/%s", pod.Namespace, pod.Name)
 }
 
-func (s *Scheduler) listNodes(stopCh chan struct{}) {
+func (s *Scheduler) listNodes(stopCh chan struct{}) listerV1.NodeLister {
 	factory := informers.NewSharedInformerFactory(s.k8sClient, 0)
-	informer := factory.Core().V1().Nodes().Informer()
+	nodes := factory.Core().V1().Nodes()
+	informer := nodes.Informer()
 
 	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc:    s.nodeAdded,
@@ -98,6 +102,7 @@ func (s *Scheduler) listNodes(stopCh chan struct{}) {
 	})
 
 	go informer.Run(stopCh)
+	return nodes.Lister()
 }
 
 func (s *Scheduler) nodeAdded(obj interface{}) {
