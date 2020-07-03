9582b87bb6861ba3771212989be5b9763bd4feae
Anbang Hu <anbhu@microsoft.com>
2020-07-03 00:25:48 +0000

    add test script

---
+++ src/docker-images/allowlist-manager/deny.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from main import RestUtil
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--user",
+                        "-u",
+                        required=True)
+    parser.add_argument("--rest",
+                        "-r",
+                        required=True)
+    args = parser.parse_args()
+
+    rest_util = RestUtil({"rest_url": args.rest})
+    rest_util.delete_allow_record(args.user)
+
