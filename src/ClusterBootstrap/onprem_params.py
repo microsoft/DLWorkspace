629f2a7db892c0565673091dd7e48469d4dfeadc
YinYangOfDao <zhexu@microsoft.com>
2020-07-14 14:46:21 -0700

    ctl.py partially supports onpremise cluster maintenance (#1250)

---
+++ src/ClusterBootstrap/onprem_params.py
@@ -0,0 +1,6 @@
+default_onprem_parameters = {
+    "sku_mapping": {
+        'DGX-1': {'cpu': 80, 'memory': 512, 'gpu': 8, 'gpu_memory': 256, 'gpu-type': 'V100'}, 
+        'DGX-2': {'cpu': 96, 'memory': 1536, 'gpu': 16, 'gpu_memory': 512, 'gpu-type': 'V100'}
+    }
+}
